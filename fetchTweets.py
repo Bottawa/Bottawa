8f8564461481167bd3b9f6f9d8275408065e0c12
gregology <greg.clarke@jadedpixel.com>
2013-11-07 17:38:30 -0500

    added logging and loop

--- fetchTweets.py
+++ fetchTweets.py
@@ -1,14 +1,26 @@
-import json, yaml#, tweetpony
+import logging, time, json, yaml#, tweetpony
+
+logger = logging.getLogger('fetchTweets')
+hdlr = logging.FileHandler('./log/fetchTweets.log')
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+hdlr.setFormatter(formatter)
+logger.addHandler(hdlr)
+logger.setLevel(logging.DEBUG)
+
+logger.info('Start time')
 
 #config = json.load(open('config.json'))
 
 POIs = yaml.load(open("twitter.yaml", 'r'))['POIs']
-
-for POI, areas in POIs.iteritems():
-    print POI, areas
+logger.debug('loaded twitter yaml')
 
 for POI in POIs:
-    print POI, POIs[POI]
+    print POI
+    for area in POIs[POI]['areas']:
+        print area['lat']
+        print area['long']
+        print area['range']
+        time.sleep(0.5)
 
 #api = tweetpony.API(consumer_key = config[u'consumer_key'], consumer_secret = config[u'consumer_secret'], access_token = config[u'access_token'], access_token_secret = config[u'access_token_secret'])
 
